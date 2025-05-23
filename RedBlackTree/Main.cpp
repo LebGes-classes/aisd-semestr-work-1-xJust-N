c7d4d7c1ea883ba76c28f81902f3b058b404190f
Just_N <86079830+xJust-N@users.noreply.github.com>
2025-05-23 12:00:03 +0300

    refactor: changed random number in main

--- RedBlackTree/Main.cpp
+++ RedBlackTree/Main.cpp
@@ -10,12 +10,10 @@ int main()
 	RedBlackTree* rbt = new RedBlackTree();
 
 	int elementsCount = 10 + rand() % 30;
-	int randomNumber;
 
 	for (int i = 0; i < elementsCount; i++)
 	{
-		randomNumber = rand() % 100;
-		rbt->add(randomNumber);
+		rbt->add(rand() % 100);
 	}
 	rbt->printInfo();
 
